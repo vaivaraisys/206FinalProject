e3accdc6a29ecbd5cdc8787524fe557961510e89
Vaiva Raisys <vaivar@umich.edu>
2024-04-19 17:07:40 -0400

    edited meal data, drink data and added data base

--- FinalProject.py
+++ FinalProject.py
@@ -38,55 +38,38 @@ def retrieve_desserts():
         for name in name_list:
             dessert_name = name.text
             dessert_titles.append(dessert_name)
-        print(len(dessert_titles))
+        #print(len(dessert_titles))
         return dessert_titles
     else:
         print("Invalid URL")
      
 def get_meal_data():
-    '''
-    creates API request
-    ARGUMENTS: 
-        title: title of the movie you're searching for 
-
-    RETURNS: 
-        tuple with the response text and url OR None if the 
-        request was unsuccesful
-    '''
-    randomLetter = random.choice(string.ascii_letters)
+    # randomLetter = random.choice(string.ascii_letters)
     # print(randomLetter)
-    url = f"https://www.themealdb.com/api/json/v1/1/search.php?f={randomLetter}"
-
-    response = requests.get(url)
-    # print(response)
-
-    if response.status_code == 200:
-        response_dict = json.loads(response.text)
-        # print(response_dict)
-            # if response_dict["Response"] == "False":
-            #     return None
-    # else:
-    #     pass
     name_id_list = []
-    for meal, meal_details in response_dict.items():
-        if meal_details == None:
-            continue
-        # print(meal_details) 
-        else:
-            for meal_data in meal_details:
-                # print(meal_data)  
-                meal_name = meal_data["strMeal"]
-                # print(meal_name)   
-                meal_id = meal_data["idMeal"]   
-                # print(meal_id)
-                meal_info = (meal_name, meal_id)
-                name_id_list.append(meal_info)
+    alphabet = [chr(i) for i in range(65, 91)]
+    for letter in alphabet:
+        # print(letter)
+
+        url = f"https://www.themealdb.com/api/json/v1/1/search.php?f={letter}"
+
+        response = requests.get(url)
+        # print(response)
+        if response.status_code == 200:
+            response_dict = json.loads(response.text)
+            meals = response_dict.get("meals")  # Get the list of meals or None
+            if meals is not None:
+                for meal_data in meals:
+                    meal_name = meal_data["strMeal"]
+                    meal_id = meal_data["idMeal"]
+                    meal_info = (meal_name, meal_id)
+                    name_id_list.append(meal_info)
     # print(name_id_list)
     return name_id_list
-    # print(name_id_list)
-        # for meal_detail in meal_details:
-            # print(meal_detail)
-    # return (response_dict, name_id_list, url)
+        # print(name_id_list)
+            # for meal_detail in meal_details:
+                # print(meal_detail)
+        # return (response_dict, name_id_list, url)
 
 # Sets up the data base 
 def set_up_database(db_name):
@@ -133,35 +116,47 @@ def set_up_types_table(meal_tuple, cur, conn):
 
 #Getting drinks id and instructions on how to make the drink
 def get_drink_data():
-    randomLetter = random.choice(string.ascii_letters)
-    # print(randomLetter)
-    url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={randomLetter}"
-
-    response = requests.get(url)
-    print(response)
-
-    if response.status_code == 200:
-        response_dict = json.loads(response.text)
-        # print(response_dict)
-    else:
-        # print(f"Error: {response.status_code} {response.reason}")
-        return None
-
+    alphabet = [chr(i) for i in range(65, 91)]
     name_id_list = []
-    for drink, drink_details in response_dict.items():
-        # print(meal_details) 
-        for drink_data in drink_details:
-            # print(meal_data)  
-            drink_instructions = drink_data["strInstructions"]
-            # print(meal_name)   
-            drink_id = drink_data["idDrink"]   
-            # print(meal_id)
-            drink_info = (drink_id, drink_instructions)
-            name_id_list.append(drink_info)
-    # print(name_id_list)
-        # for meal_detail in meal_details:
-            # print(meal_detail)
-    return (response_dict, name_id_list, url)
+
+    for letter in alphabet:
+        url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={letter}"
+
+        response = requests.get(url)
+        print(response)
+        if response.status_code == 200:
+            response_dict = json.loads(response.text)
+            drinks = response_dict.get("drinks")  # Get the list of meals or None
+            if drinks is not None:
+                for drink_data in drinks:
+                    drink_instructions = drink_data["strInstructions"]
+                    drink_id = drink_data["idDrink"]
+                    drink_info = (drink_instructions, drink_id)
+                    name_id_list.append(drink_info)
+        # if response.status_code == 200:
+        #     response_dict = json.loads(response.text)
+        #     # print(response_dict)
+        # else:
+        #     # print(f"Error: {response.status_code} {response.reason}")
+        #     return None
+
+        # name_id_list = []
+        # for drink, drink_details in response_dict.items():
+        #     # print(meal_details) 
+        #     for drink_data in drink_details:
+        #         # print(meal_data)  
+        #         drink_instructions = drink_data["strInstructions"]
+        #         # print(meal_name)   
+        #         drink_id = drink_data["idDrink"]   
+        #         # print(meal_id)
+        #         drink_info = (drink_id, drink_instructions)
+        #         name_id_list.append(drink_info)
+        # print(name_id_list)
+            # for meal_detail in meal_details:
+                # print(meal_detail)
+    print(name_id_list)
+    return name_id_list
+    # return (response_dict, name_id_list, url)
 
 
 
@@ -208,8 +203,8 @@ def main():
         
     # DO NOT CHANGE THIS 
     #######################################
-    # meal_dict = get_meal_data()
-    # drink_dict = get_drink_data()
+    meal_dict = get_meal_data()
+    drink_dict = get_drink_data()
     desserts = retrieve_desserts()
     # country_names = retrieve_countries("AlphabeticalCountries.html")
     # print(meal_dict)
